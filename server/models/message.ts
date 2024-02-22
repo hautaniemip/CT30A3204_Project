84efa7c6b871bc416d9a11c60c63efbdce64467c
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-22 13:17:30 +0200

    Server: Removed redundant field from Message model

--- server/models/message.ts
+++ server/models/message.ts
@@ -3,7 +3,6 @@ import mongoose, {Schema} from 'mongoose';
 const messageSchema = new mongoose.Schema({
     chat: {type: Schema.Types.ObjectId, required: true},
     sender: {type: Schema.Types.ObjectId, required: true},
-    receiver: {type: Schema.Types.ObjectId, required: true},
     content: {type: String, required: true},
 }, {timestamps: true});
 
