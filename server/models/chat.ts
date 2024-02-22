c766ab79722e5e7baefd2a633884cce091f82443
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-22 23:57:15 +0200

    Server: Added pre-save hook to "Chat" model for updating timestamp

--- server/models/chat.ts
+++ server/models/chat.ts
@@ -4,4 +4,12 @@ const chatSchema = new mongoose.Schema({
     participants: [Schema.Types.ObjectId],
 }, {timestamps: true});
 
+chatSchema.pre('save', function () {
+    const changes = this.getChanges();
+
+    if (Object.keys(changes).length === 0) {
+        this.set('updatedAt', new Date());
+    }
+});
+
 export const Chat = mongoose.model('Chat', chatSchema);
