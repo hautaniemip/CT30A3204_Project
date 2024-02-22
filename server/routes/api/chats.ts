d0e76f7886b351a43ff1c32ce2ccc4c325a0a081
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-22 23:56:09 +0200

    Server: Added route to get chat info

--- server/routes/api/chats.ts
+++ server/routes/api/chats.ts
@@ -4,6 +4,7 @@ import passport from 'passport';
 
 import {User} from '../../models/user';
 import {Chat} from '../../models/chat';
+import {Message} from '../../models/message';
 
 export const router = express.Router();
 
@@ -21,6 +22,48 @@ router.get('/', passport.authenticate('jwt'), (req, res) => {
         Chat.find({participants: user._id}, {}, {sort: {updatedAt: -1}}).then((chats) => {
             console.log(chats);
             res.json({chats: chats});
+        });
+    });
+});
+
+router.get('/:chat', passport.authenticate('jwt'), (req, res) => {
+    const token = req.cookies['access_token'];
+    const decoded_token = jwt.verify(token, process.env.SECRET) as JwtPayload;
+    const id = decoded_token._id;
+    const chatId = req.params.chat;
+
+    User.findById(id).then((user) => {
+        if (!user) {
+            res.status(400).send();
+            return;
+        }
+
+        Chat.findById(chatId).then(async (chat) => {
+            if (!chat) {
+                res.status(400).send();
+                return;
+            }
+
+            let otherUserId = null;
+            for (const participant of chat.participants) {
+                if (participant.toString() === id)
+                    continue;
+                otherUserId = participant;
+            }
+
+            const otherUser = await User.findById(otherUserId);
+            const lastMessage = await Message.findOne({chat: chatId}, {}, {sort: {createdAt: -1}});
+
+            if (!otherUser) {
+                res.status(400).send();
+                return;
+            }
+            res.json({
+                name: otherUser.name,
+                time: lastMessage ? lastMessage.createdAt : null,
+                sender: lastMessage && lastMessage.sender.toString() === id ? 'You' : otherUser.name.split(' ')[0],
+                message: lastMessage ? lastMessage.content : null
+            })
         });
     });
 });
