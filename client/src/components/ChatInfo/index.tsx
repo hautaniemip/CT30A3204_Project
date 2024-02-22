09464793b9a14f41648f7d713ec44f4425312914
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-22 23:42:43 +0200

    Components/ChatInfo: Added component for displaying info about chats

---
+++ client/src/components/ChatInfo/index.tsx
@@ -0,0 +1,50 @@
+import {useEffect, useState} from "react";
+import "./ChatInfo.css"
+
+type ChatProps = {
+    chatId?: string | null;
+}
+
+type Chat = {
+    name: string;
+    time: string | null;
+    sender: string | null
+    message: string | null;
+}
+
+const ChatInfo = ({chatId = null}: ChatProps) => {
+    const [chatInfo, setChatInfo] = useState<Chat | null>(null);
+
+    useEffect(() => {
+        if (!chatId)
+            return;
+
+        fetch("/api/chats/" + chatId).then((res) => {
+            if (res.status !== 200) {
+                throw new Error("Invalid request")
+            }
+            return res.json();
+        }).then((data) => setChatInfo(data)).catch((err) => console.error(err));
+    }, [chatId]);
+
+    if (!chatInfo)
+        return;
+
+    return (
+        <div className={"chat-info"}>
+            <div className={"row"}>
+                <span className={"name"}>
+                    {chatInfo.name}
+                </span>
+                <span className={"timestamp"}>
+                    {chatInfo.time ? new Intl.DateTimeFormat("fi").format(new Date(chatInfo.time)) : ""}
+                </span>
+            </div>
+            <div>
+                <span>{chatInfo.message}</span>
+            </div>
+        </div>
+    )
+}
+
+export default ChatInfo;
