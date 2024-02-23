ced589f0c26a6cdd30e0d6c52ecde5e6814c00e9
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-23 16:55:31 +0200

    Page/Register: Added a new page for account registration

---
+++ client/src/pages/RegisterPage/index.tsx
@@ -0,0 +1,33 @@
+import {Form, useActionData, useNavigation} from "react-router-dom";
+
+const RegisterPage = () => {
+    let navigation = useNavigation();
+    let isRegistering = navigation.formData?.get("email") != null;
+
+    let actionData = useActionData() as { error: string } | undefined;
+
+    return (
+        <>
+            <h2>Register</h2>
+            <Form method="post" replace>
+                <label>
+                    Name: <input name="name"/>
+                </label>{" "}
+                <label>
+                    Email: <input name="email"/>
+                </label>{" "}
+                <label>
+                    Password: <input name="password"/>
+                </label>{" "}
+                <button type="submit" disabled={isRegistering}>
+                    {isRegistering ? "Registering..." : "Register"}
+                </button>
+                {actionData && actionData.error ? (
+                    <p style={{color: "red"}}>{actionData.error}</p>
+                ) : null}
+            </Form>
+        </>
+    );
+}
+
+export default RegisterPage;
