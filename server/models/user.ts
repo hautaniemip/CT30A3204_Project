157ba162469829bd992fd47ecdc22e983dd3c78a
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-24 00:35:02 +0200

    Server: Added status text to user model

--- server/models/user.ts
+++ server/models/user.ts
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
+    status: String,
     liked: [Schema.Types.ObjectId],
     likedBy: [Schema.Types.ObjectId],
     matches: [Schema.Types.ObjectId],
