52a2bf0d626b9d138f545e3bbd8ea04897d6c489
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-23 18:24:47 +0200

    Pages/Account: Added fields for user editing

--- client/src/pages/AccountPage/index.tsx
+++ client/src/pages/AccountPage/index.tsx
@@ -1,6 +1,46 @@
+import {useEffect, useState} from "react";
+import {AuthProvider} from "../../components/AuthProvider";
+
 const AccountPage = () => {
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+
+    useEffect(() => {
+        fetch("/api/users/" + AuthProvider.id).then((res) => {
+            if (res.status !== 200)
+                return;
+            return res.json();
+        }).then((data) => {
+            setName(data.name);
+            setEmail(data.email);
+        });
+    }, []);
+
+    const updateProfile = () => {
+        fetch("/api/users/edit", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({name, email}),
+        });
+    }
+
     return (
-        <h3>Account</h3>
+        <>
+            <h3>Account</h3>
+            <div>
+                <label>
+                    Name: <input name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+                </label>{" "}
+                <label>
+                    Email: <input name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </label>{" "}
+                <button type="submit" onClick={updateProfile}>
+                    Save
+                </button>
+            </div>
+        </>
     );
 }
 
