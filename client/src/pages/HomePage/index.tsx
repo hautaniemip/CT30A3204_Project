11af0e0e1d6759d5ac436d1dc16a748f2e23f630
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-23 12:51:59 +0200

    Pages/Home: Added ChatManager to home page

--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -1,7 +1,10 @@
+import ChatManager from "../../components/ChatManager";
+
 const HomePage = () => {
     return (
         <>
             <h2>Home</h2>
+            <ChatManager/>
         </>
     );
 }
