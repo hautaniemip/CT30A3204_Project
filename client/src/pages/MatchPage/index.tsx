fbb3331ed0a45217badc6bd99b0fdd4bfb393c79
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-23 19:59:57 +0200

    Page/Match: Added a way to get and display random user

--- client/src/pages/MatchPage/index.tsx
+++ client/src/pages/MatchPage/index.tsx
@@ -1,6 +1,33 @@
+import {useEffect, useState} from "react";
+import {User} from "../../types/user";
+
 const MatchPage = () => {
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        fetch("/api/users/random").then((res) => {
+            if (res.status !== 200)
+                return;
+            return res.json();
+        }).then((data) => setUser(data));
+    }, []);
+
+    const likeUser = (id: string) => {
+        fetch("/api/users/like", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({id: id}),
+        });
+    }
+
     return (
-        <h3>Matches</h3>
+        <>
+            <h3>Find matches</h3>
+            {user && user.id !== "0" && <span onClick={() => likeUser(user.id)}>{user.name}</span>}
+            {user && user.id === "0" && <span>No new users</span>}
+        </>
     );
 }
 
