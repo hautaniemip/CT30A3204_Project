ee3f66df6071bb8dbbf3605291091b5121643661
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-24 22:28:40 +0200

    Page/Register: Switched to use RegisterForm component

--- client/src/pages/RegisterPage/index.tsx
+++ client/src/pages/RegisterPage/index.tsx
@@ -1,31 +1,10 @@
-import {Form, useActionData, useNavigation} from "react-router-dom";
+import RegisterForm from "../../components/RegisterForm";
 
 const RegisterPage = () => {
-    let navigation = useNavigation();
-    let isRegistering = navigation.formData?.get("email") != null;
-
-    let actionData = useActionData() as { error: string } | undefined;
-
     return (
         <>
             <h2>Register</h2>
-            <Form method="post" replace>
-                <label>
-                    Name: <input name="name"/>
-                </label>{" "}
-                <label>
-                    Email: <input name="email" type="email"/>
-                </label>{" "}
-                <label>
-                    Password: <input name="password" type="password"/>
-                </label>{" "}
-                <button type="submit" disabled={isRegistering}>
-                    {isRegistering ? "Registering..." : "Register"}
-                </button>
-                {actionData && actionData.error ? (
-                    <p style={{color: "red"}}>{actionData.error}</p>
-                ) : null}
-            </Form>
+            <RegisterForm/>
         </>
     );
 }
