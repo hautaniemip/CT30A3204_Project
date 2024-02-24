3610d2afdcf330dfba00abbd818da35ab1e505f5
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-24 03:04:11 +0200

    Page/Match: Switched to UserCard component

--- client/src/pages/MatchPage/index.tsx
+++ client/src/pages/MatchPage/index.tsx
@@ -1,34 +1,12 @@
-import {useEffect, useState} from "react";
-import {User} from "../../types/user";
+import UserCard from "../../components/UserCard";
 
 const MatchPage = () => {
-    const [user, setUser] = useState<User | null>(null);
-
-    useEffect(() => {
-        fetch("/api/users/random").then((res) => {
-            if (res.status !== 200)
-                return;
-            return res.json();
-        }).then((data) => setUser(data));
-    }, []);
-
-    const likeUser = (id: string) => {
-        fetch("/api/users/like", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({id: id}),
-        });
-    }
-
     return (
         <>
             <div className={"sub-header"}>
                 <h3>Find matches</h3>
             </div>
-            {user && user.id !== "0" && <span onClick={() => likeUser(user.id)}>{user.name}</span>}
-            {user && user.id === "0" && <span>No new users</span>}
+            <UserCard/>
         </>
     );
 }
