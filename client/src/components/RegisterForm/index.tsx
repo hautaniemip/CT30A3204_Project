3fe02e46ba853251e656e2f04936084570dd607e
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-24 22:27:58 +0200

    Components/RegisterForm: Added RegisterForm component

---
+++ client/src/components/RegisterForm/index.tsx
@@ -0,0 +1,30 @@
+import {Form, useActionData, useNavigation} from "react-router-dom";
+
+const RegisterForm = () => {
+    let navigation = useNavigation();
+    let isRegistering = navigation.formData?.get("email") != null;
+
+    let actionData = useActionData() as { error: string } | undefined;
+
+    return (
+        <Form method="post" replace>
+            <label>
+                Name: <input name="name"/>
+            </label>{" "}
+            <label>
+                Email: <input name="email" type="email"/>
+            </label>{" "}
+            <label>
+                Password: <input name="password" type="password"/>
+            </label>{" "}
+            <button type="submit" disabled={isRegistering}>
+                {isRegistering ? "Registering..." : "Register"}
+            </button>
+            {actionData && actionData.error ? (
+                <p style={{color: "red"}}>{actionData.error}</p>
+            ) : null}
+        </Form>
+    );
+}
+
+export default RegisterForm;
