cbda30a6bf3605b907114c31e9b01c7a2a43708e
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-02-24 22:40:32 +0200

    Page/Login: Switched to use LoginForm component

--- client/src/pages/LoginPage/index.tsx
+++ client/src/pages/LoginPage/index.tsx
@@ -1,31 +1,12 @@
-import {Form, Link, useActionData, useNavigation} from "react-router-dom";
+import LoginForm from "../../components/LoginForm";
 
 const LoginPage = () => {
-    let navigation = useNavigation();
-    let isLoggingIn = navigation.formData?.get("email") != null;
-
-    let actionData = useActionData() as { error: string } | undefined;
-
     return (
         <>
             <div className={"sub-header"}>
                 <h3>Login</h3>
             </div>
-            <Form method="post" replace>
-                <label>
-                    Email: <input name="email" type="email"/>
-                </label>{" "}
-                <label>
-                    Password: <input name="password" type="password"/>
-                </label>{" "}
-                <button type="submit" disabled={isLoggingIn}>
-                    {isLoggingIn ? "Logging in..." : "Login"}
-                </button>
-                {actionData && actionData.error ? (
-                    <p style={{color: "red"}}>{actionData.error}</p>
-                ) : null}
-            </Form>
-            <Link to={"/register"}>Don't have a account yet?</Link>
+            <LoginForm/>
         </>
     );
 }
